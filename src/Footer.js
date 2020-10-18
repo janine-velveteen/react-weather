f79ec09c3415ca918e880ad9a51bc84a2a0f3e34
janine-velveteen <68258844+janine-velveteen@users.noreply.github.com>
2020-10-18 17:41:25 -0400

    Updated CSS

--- src/Footer.js
+++ src/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
         <footer>
           Life in the Clouds
           <br />
-          <a href="https://github.com/janine-velveteen/react-weather">
+          <a href="https://github.com/janine-velveteen/react-weather" target="_blank" rel="noopener noreferrer">
             Open-Sourced Code
           </a>{" "}
           by Janine Marsh
